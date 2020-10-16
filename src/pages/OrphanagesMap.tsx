430d2c06c5abce215396a0f4e0f51d34300eefae
Maikon Germano <mk_germano@hotmail.com>
2020-10-16 13:59:11 -0300

    feat: add navigate OrphanagesMap to SelectMapPosition

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -15,6 +15,10 @@ export default function OrphanagesMap () {
         navigation.navigate('OrphanagesDetails')
     }
 
+    function handleNavigateToCreateOrphanage() {
+      navigation.navigate('SelectMapPosition')
+    }
+
     return (
         <View style={styles.container}>
         <MapView 
@@ -51,7 +55,7 @@ export default function OrphanagesMap () {
           <Text style={styles.footerText}>
             2 orfanatos encontrados
           </Text>
-          <TouchableOpacity style={styles.createOrphanageButton} onPress={() =>{}}>
+          <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
             <Feather name="plus" size={20} color='#FFF'/>
           </TouchableOpacity>
         </View>
