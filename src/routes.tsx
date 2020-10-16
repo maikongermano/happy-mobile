ca96902d8b2e924b753c7846a8bcc920993ea2c3
Maikon Germano <mk_germano@hotmail.com>
2020-10-16 14:29:55 -0300

    feat: add new pages on app

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 const { Navigator, Screen } = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap';
-import OrphanagesDetails from './pages/OrphanagesDetails';
+import OrphanageDetails from './pages/OrphanageDetails';
 
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import OrphanageData    from './pages/CreateOrphanage/OrphanageData';
@@ -18,8 +18,8 @@ export default function Routes() {
             <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5'} }}>
                 <Screen name="OrphanagesMap" component={OrphanagesMap} />
                 <Screen 
-                    name="OrphanagesDetails" 
-                    component={OrphanagesDetails} 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails} 
                     options={{
                         headerShown: true,
                         header: () => <Header showCancel={false} title="Orfanato" />
